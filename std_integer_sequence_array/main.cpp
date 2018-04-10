e4554afb76ff1c9c07a2ff6c13d2492543593b1c
Abu Muhammad <com.koloboot@gmail.com>
2018-04-10 11:14:03 -0700

    Initial Commit

---
+++ std_integer_sequence_array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  std_integer_sequence_array
+//
+//  Created by Abu Muhammad on 10/04/18.
+//  Copyright © 2018 Abu Muhammad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
